b7908a2203aa713a1228c94b2e48a83df45baa8f
Alexey Vasiliev <leopard.not.a@gmail.com>
2013-12-22 03:41:16 +0200

    new rubies

--- site-cookbooks/rwprecise64/recipes/pre-hooks.rb
+++ site-cookbooks/rwprecise64/recipes/pre-hooks.rb
@@ -1,6 +1,6 @@
 package "curl"
 package "virtualbox-guest-utils"
-
+=begin
 script "update chef" do
   code <<-EOH
   /opt/vagrant_ruby/bin/gem install chef --no-rdoc --no-ri
@@ -10,4 +10,4 @@ script "update chef" do
   user "root"
   action :run
 end
-
+=end
