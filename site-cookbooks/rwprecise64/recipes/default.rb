2ee8b24a30f0cbb66ca47d5d5bb804d90f0cd8f1
Alexey Vasiliev <leopard.not.a@gmail.com>
2014-07-31 15:30:57 +0300

    fix locale

--- site-cookbooks/rwprecise64/recipes/default.rb
+++ site-cookbooks/rwprecise64/recipes/default.rb
@@ -26,6 +26,33 @@ package "libcurl4-openssl-dev"
 # utils
 package "htop"
 
+# UTF-8 locale
+locale_vars = <<EOF
+LANG=en_US.UTF-8
+LANGUAGE=en_US.UTF-8
+LC_CTYPE="en_US.UTF-8"
+LC_NUMERIC="en_US.UTF-8"
+LC_TIME="en_US.UTF-8"
+LC_COLLATE="en_US.UTF-8"
+LC_MONETARY="en_US.UTF-8"
+LC_MESSAGES="en_US.UTF-8"
+LC_PAPER="en_US.UTF-8"
+LC_NAME="en_US.UTF-8"
+LC_ADDRESS="en_US.UTF-8"
+LC_TELEPHONE="en_US.UTF-8"
+LC_MEASUREMENT="en_US.UTF-8"
+LC_IDENTIFICATION="en_US.UTF-8"
+LC_ALL=en_US.UTF-8
+EOF
+
+file "/etc/default/locale" do
+  content locale_vars
+  owner "root"
+  group "root"
+  mode 00644
+end
+
+
 
 include_recipe 'rwprecise64::mysql'
 include_recipe 'rwprecise64::postgresql'
